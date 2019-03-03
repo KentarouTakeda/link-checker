3a44a36f5f65708441c7ff1b6cca6146a818ef80
武田 憲太郎 <takeda@youmind.jp>
2019-03-03 19:20:46 +0900

    再起制御用の引数は公開しない

--- src/scraping.ts
+++ src/scraping.ts
@@ -1,5 +1,9 @@
 import { parse, parseLinksFromUrl } from './parseLinks';
 
-export async function scrape(url: string, log: Function|undefined = console.info, limit: number = Infinity, i: number = 0): Promise<parse[]> {
-	return [];
+export async function scrape(url: string, limit: number = Infinity, log: Function|undefined = ()=>{}): Promise<parse[]> {
+	return f(url, limit, log, 0, []);
+}
+
+async function f(url: string, limit: number, log: Function|undefined, i: number, result: parse[]): Promise<parse[]> {
+	return result;
 }
