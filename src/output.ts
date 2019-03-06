39a7a272810649fcf3e876e00c340a81b694234a
武田 憲太郎 <takeda@youmind.jp>
2019-03-06 20:52:58 +0900

    outputの返却要素数を型定義で固定する

--- src/output.ts
+++ src/output.ts
@@ -1,41 +1,39 @@
 import { parse } from '../src/parseLinks';
 import URL = require('url');
 
-export function summary(_r: parse[]): (string|null)[][] {
+type SummaryRow = [string, string, string, string]
+export function summary(_r: parse[]): SummaryRow[] {
 	const result: parse[] = JSON.parse(JSON.stringify(_r));
 
-	return result.sort((a, b) => sortString(a.url, b.url)).map(v => [
+	return result.sort((a, b) => sortString(a.url, b.url)).map<SummaryRow>(v => [
 		v.url,
-		v.title,
-		v.canonical,
-		v.description,
+		v.title||'',
+		v.canonical||'',
+		v.description||'',
 	]);
 }
 
-export function links(_r: parse[]): (string|null)[][] {
+type LinksRow = [string, string, string, string, string, string, string, string]
+export function links(_r: parse[]): LinksRow[] {
 	const result: parse[] = JSON.parse(JSON.stringify(_r));
-	const ret : (string|null)[][] = [];
+	const ret : LinksRow[] = [];
 
 	result.sort((a, b) => sortString(a.url, b.url));
 	result.forEach(v => {
 		const urlBase = URL.parse(v.url);
-
 		v.links.forEach(link => {
-			const row: (string|null)[] = [];
 			const urlLink = URL.parse(link.prop)
-
-			row.push(v.url);
-			row.push(link.attr);
-			row.push(link.prop);
-			row.push(urlLink.protocol || '');
-			row.push(urlLink.host || '');
-			row.push(urlLink.pathname || '');
-			row.push(urlLink.pathname!.substr(-1, 1)==='/' ? '1' : '0');
-			row.push(urlLink.search || '')
-
-			ret.push(row);
+			ret.push([
+				v.url,
+				link.attr,
+				link.prop,
+				urlLink.protocol || '',
+				urlLink.host || '',
+				urlLink.pathname || '',
+				urlLink.pathname!.substr(-1, 1)==='/' ? '1' : '0',
+				urlLink.search || '',
+			]);
 		});
-
 	});
 
 	return ret;
